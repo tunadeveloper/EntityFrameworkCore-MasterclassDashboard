40d94efcc4d1d06baf39153a2193e86a5d9ff159
tunadeveloper <tunabusiness25@gmail.com>
2026-03-10 18:04:45 +0300

    Updated

---
+++ EntityFrameworkCore-MasterclassDashboard/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using EntityFrameworkCore_MasterclassDashboard.Context;
+using EntityFrameworkCore_MasterclassDashboard.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCore_MasterclassDashboard.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MasterclassContext _context;
+
+        public ProductController(MasterclassContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.Products.ToList();
+            return View(values);
+        }
+
+        public IActionResult CreateProduct() => View();
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            var values = _context.Products.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var values = _context.Products.Find(id);
+            _context.Products.Remove(values);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
