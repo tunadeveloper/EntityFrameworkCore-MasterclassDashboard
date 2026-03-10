5df7ecb1a444e3edae624552e1882e0730c23f00
tunadeveloper <tunabusiness25@gmail.com>
2026-03-10 22:40:27 +0300

    Updated

--- EntityFrameworkCore-MasterclassDashboard/Controllers/ProductController.cs
+++ EntityFrameworkCore-MasterclassDashboard/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
-﻿using EntityFrameworkCore_MasterclassDashboard.Context;
+using EntityFrameworkCore_MasterclassDashboard.Context;
 using EntityFrameworkCore_MasterclassDashboard.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using X.PagedList.Extensions;
 
 namespace EntityFrameworkCore_MasterclassDashboard.Controllers
 {
@@ -13,25 +17,36 @@ namespace EntityFrameworkCore_MasterclassDashboard.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? page)
         {
-            var values = _context.Products.ToList();
+            int pageNumber = page ?? 1;
+            var values = _context.Products.Include(x=>x.Category).OrderBy(x=>x.Id).ToPagedList(pageNumber, 10);
+
+            ViewBag.totalProduct = _context.Products.Count().ToString("N0");
+            ViewBag.totalStock = _context.Products.Sum(x=>x.ProductStock).ToString("N0");
+            ViewBag.avgPrice = _context.Products.Average(x => x.ProductPrice).ToString("N2");
             return View(values);
         }
 
-        public IActionResult CreateProduct() => View();
+        public IActionResult CreateProduct()
+        {
+            ViewBag.categoryList = new SelectList(_context.Categories.ToList(), "Id", "CategoryName");
+            return View();
+        }
 
         [HttpPost]
         public IActionResult CreateProduct(Product product)
         {
             _context.Products.Add(product);
             _context.SaveChanges();
+            ViewBag.categoryList = new SelectList(_context.Categories.ToList(), "Id", "CategoryName");
             return RedirectToAction(nameof(Index));
         }
 
         public IActionResult UpdateProduct(int id)
         {
             var values = _context.Products.Find(id);
+            ViewBag.categoryList = new SelectList(_context.Categories.ToList(), "Id", "CategoryName", values.CategoryId);
             return View(values);
         }
 
@@ -40,6 +55,7 @@ namespace EntityFrameworkCore_MasterclassDashboard.Controllers
         {
             _context.Products.Update(product);
             _context.SaveChanges();
+            ViewBag.categoryList = new SelectList(_context.Categories.ToList(), "Id", "CategoryName");
             return RedirectToAction(nameof(Index));
         }
 
